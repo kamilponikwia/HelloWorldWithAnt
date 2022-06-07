6aa43e5eea20af65f6ff564d94fc91f43f57c312
Kamil Ponikwia <kamil.ponikwia@gmail.com>
2022-06-07 10:36:10 +0200

    Added log4J

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -1,7 +1,11 @@
 package oata;
 
+import org.apache.log4j.Logger;
+
 public class HelloWorld {
+    static Logger logger = Logger.getLogger(HelloWorld.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        logger.info("Hello World");          // the old SysO-statement
     }
 }
